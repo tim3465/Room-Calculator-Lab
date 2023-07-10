8e3aaff5261c9e454147f33af184f206540c8d74
Tim Montague <tim.john.montague@gmail.com>
2023-07-10 14:40:32 -0400

    Added comments

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
-﻿
-
-
-
+﻿// User entry 
 Console.WriteLine("Welcome to Grand Circus’ Room Detail Generator!");
 Console.WriteLine("Enter the length of the room ");
 double length=double.Parse(Console.ReadLine());
@@ -10,15 +7,13 @@ double width=double.Parse(Console.ReadLine());
 Console.WriteLine("Enter the height ");
 double  height=double.Parse(Console.ReadLine());
 
+//Mathlogic 
 double squarefeet= length*width;
 double perimeter= (length*2)+(width*2);
 double volume = squarefeet * height;
 double surfaceArea=(squarefeet*2)+((height*length)*2)+((height*width)*2);
 
-
-
-
-
+//output 
 Console.WriteLine($"Area: {squarefeet}");
 Console.WriteLine($"Perimeter: {perimeter}");
 Console.WriteLine($"Volume: {volume}");
